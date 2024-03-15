a26e438b8259960126aec29eee939355f8c51b4b
SethuJaganathaan <sethuj99@gmail.com>
2024-03-15 13:32:53 +0530

    Client-app created

---
+++ client-app/src/App.tsx
@@ -0,0 +1,14 @@
+import { Card, CardHeader, Container } from 'semantic-ui-react'
+
+function App() {
+
+  return (
+    <Card>
+      <CardHeader>
+        Hello Senpai
+      </CardHeader>
+    </Card>
+  )
+}
+
+export default App
---
+++ client-app/src/main.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import 'semantic-ui-css/semantic.min.css'
+import App from './App.tsx'
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
