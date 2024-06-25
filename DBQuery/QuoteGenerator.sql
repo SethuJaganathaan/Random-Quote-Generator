0d3cd467689e6466de7e84456812a250407b1d7a
SethuJaganathaan <sethuj99@gmail.com>
2024-06-25 21:03:32 +0530

    db query

---
+++ DBQuery/QuoteGenerator.sql
@@ -0,0 +1,20 @@
+CREATE TABLE Quotes (
+    QuoteId UNIQUEIDENTIFIER PRIMARY KEY,
+    QuoteText NVARCHAR(MAX) NOT NULL,
+    Author NVARCHAR(100),
+    CreatedAt DATETIME DEFAULT GETDATE()
+);
+
+INSERT INTO Quotes (QuoteId ,QuoteText, Author)
+VALUES 
+(NEWID(),'Hardest Choice requires the strongest will', 'Thanos'), 
+(NEWID(),'Be Loyal to what Matters', 'Arthur Morgan'),
+(NEWID(),'Men are Brave', 'The Batman')
+
+INSERT INTO Quotes (QuoteId ,QuoteText, Author)
+VALUES 
+(NEWID(),'Men are Brave', 'The Batman');
+
+SELECT * FROM Quotes
+
+
